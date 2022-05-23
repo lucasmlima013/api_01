a2a374b567311a96a3f7303e5e3edac94eff43c5
unknown <lucasmlima@gmail.com>
2022-05-23 12:39:36 -0300

    Add Header

--- Program.cs
+++ Program.cs
@@ -3,5 +3,10 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 app.MapPost("/", () => new {Name = "Lucas Marinho", Age = "35"});
+app.MapGet("/AddHeader", (HttpResponse response) => {
+    response.Headers.Add("Teste", "Lucas");
+    return "Olá";
+    });
+
 
 app.Run();
